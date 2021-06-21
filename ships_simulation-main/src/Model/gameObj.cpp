53d8574b789f5254c26d24716aebd1d1db1201fc
roni <roni148148@gmail.com>
2021-06-21 09:51:54 +0300

    merge with Omri's project

---
+++ ships_simulation-main/src/Model/gameObj.cpp
@@ -0,0 +1,8 @@
+
+#include "gameObj.h"
+#include "Boat.h"
+#include "Port.h"
+/*****************/
+// this file was generated for includings issues only.
+/*****************/
+gameObj::gameObj():update_ready(false)	{}
